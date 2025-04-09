7774fc019262cf00fb305c456a3ff49f81589e81
juan-medinaf <146041515+juan-medinaf@users.noreply.github.com>
2025-04-09 15:04:36 -0500

    Update RAG_portfolio_final-9.py

--- RAG_portfolio_final-9.py
+++ RAG_portfolio_final-9.py
@@ -12,7 +12,7 @@ import pandas as pd
 import numpy as np
 import fitz
 import re
-from sentence_transformers import SentenceTransformer
+from sentence_transformers import SentenceTransformer, models
 import torch
 
 # In[2]:
